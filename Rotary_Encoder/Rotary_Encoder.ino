bbba1b1d4bc1f777fb342f50cd3c573014ffd853
Simon Pascal Baur <sbausis@users.noreply.github.com>
2018-10-08 19:44:50 +0200

    stuff

--- Rotary_Encoder/Rotary_Encoder.ino
+++ Rotary_Encoder/Rotary_Encoder.ino
@@ -13,6 +13,7 @@ int get_rotation(long reading, long previous) {
 }
 
 int cnt = 0;
+float turns = 0;
 
 int turn_indicator_PIN = 4;
 int turn_indicator_value_new = 0;
@@ -28,12 +29,25 @@ long get_turn_indicator_new_value(long reading, long previous) {
 	if (turn_indicator_value_new != turn_indicator_value_old) {
 		if (reading > previous) {
 			cnt++;
+			Serial.print(cnt);
+			Serial.print(" +\t");
 		} else {
 			cnt--;
+			Serial.print(cnt);
+			Serial.print(" -\t");
 		}
-    new_value = (cnt * 3600);
+		new_value = (cnt * 3600);
+	} else {
+		Serial.print(cnt);
+		Serial.print("\t");
 	}
-  turn_indicator_value_old = turn_indicator_value_new;
+	
+	turns = (cnt/2);
+	Serial.print(turns);
+	Serial.print("\t");
+	Serial.print(new_value);
+	Serial.print("\t");
+	turn_indicator_value_old = turn_indicator_value_new;
 	return new_value;
 }
 
@@ -44,7 +58,7 @@ Encoder myEnc(encoder_A_PIN, encoder_B_PIN);
 
 void setup() {
 	
-	setup_turn_indicator();
+	//setup_turn_indicator();
 	
 	Serial.begin(9600);
 	Serial.println("Basic Encoder Test:");
@@ -55,14 +69,11 @@ long oldPosition  = -999;
 void loop() {
 	long newPosition = myEnc.read();
 	if (newPosition != oldPosition) {
-		get_turn_indicator_new_value(newPosition, oldPosition);
+		//get_turn_indicator_new_value(newPosition, oldPosition);
 		
-		Serial.print((cnt/2));
-		Serial.print("\t");
-   
-    Serial.print(newPosition);
-    
-		Serial.println("");                // prints another carriage return
+		Serial.print(newPosition);
+		
+		Serial.println("");
 		
 		oldPosition = newPosition;
 	}
